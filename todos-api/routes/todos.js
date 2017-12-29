77da4d5a0fd9776e96a5215e908a52febbf9c78c
EC2 Default User <ec2-user@ip-172-31-46-49.us-west-2.compute.internal>
2017-12-29 17:29:06 +0000

    Same skeletal code with no html. Added in a get request with 'findById' and a put request with 'findOneAndUpdate'.

--- todos-api/routes/todos.js
+++ todos-api/routes/todos.js
@@ -15,7 +15,27 @@ router.get('/', function(req, res){
 router.post('/', function(req, res){
     db.Todo.create(req.body)
     .then(function(newTodo){
-        res.json(newTodo);
+        res.status(201).json(newTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    });
+});
+
+router.get('/:todoId', function(req, res){
+    db.Todo.findById(req.params.todoId)
+    .then(function(foundTodo){
+        res.json(foundTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    });
+});
+
+router.put('/:todoId', function(req, res){
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+    .then(function(todo){
+        res.json(todo);
     })
     .catch(function(err){
         res.send(err);
