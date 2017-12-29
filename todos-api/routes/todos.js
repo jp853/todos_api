62537ac93806a89bdce53d7060781bf9dec9acfa
Justin Pingatore <jpingatore@gmail.com>
2017-12-29 17:34:17 +0000

    Added DELETE request to finish restful apis. Still need to refactor each request to clean up code.

--- todos-api/routes/todos.js
+++ todos-api/routes/todos.js
@@ -39,7 +39,17 @@ router.put('/:todoId', function(req, res){
     })
     .catch(function(err){
         res.send(err);
-    })
+    });
 });
+
+router.delete('/:todoId', function(req, res){
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function(){
+        res.json({message: 'We deleted it!'});
+    })
+    .catch(function(err){
+        res.send(err);
+    });
+})
 
 module.exports = router;
