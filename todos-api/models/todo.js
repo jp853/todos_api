08b2eee1f7c1d312a0130c11add73ee3f03a72a6
EC2 Default User <ec2-user@ip-172-31-46-49.us-west-2.compute.internal>
2017-12-29 17:18:15 +0000

    skeletal code for todo app. Inlcudes basic get and post request without html

---
+++ todos-api/models/todo.js
@@ -0,0 +1,23 @@
+// logic for todo schema
+
+var mongoose = require("mongoose");
+
+var todoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'Name cannot be blank'
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Compile into model
+var Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
